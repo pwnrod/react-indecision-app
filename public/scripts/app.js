6fccae5a579a748baeb01615c02a32707684e4af
Caylin James <caylinjames.dev@gmail.com>
2018-11-20 08:12:44 -0500

    conditional rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,22 +4,26 @@ console.log('App.js is running!');
 
 var app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
-
-// JSX - JavaScript XML
 var template = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        app.title
+        'Indecision App'
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
     ),
     React.createElement(
         'p',
         null,
-        app.subtitle
+        app.options.length > 0 ? "Here are your options" : "No options"
     ),
     React.createElement(
         'ol',
@@ -42,26 +46,29 @@ var user = {
     age: 28,
     location: 'Louisville, KY'
 };
+function getLocation(location) {
+    if (location) return React.createElement(
+        'p',
+        null,
+        'Location: ',
+        location
+    );
+}
 var templateTwo = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        user.name
+        user.name ? user.name : 'Anonymous'
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         'p',
         null,
         'Age: ',
         user.age
     ),
-    React.createElement(
-        'p',
-        null,
-        'Location: ',
-        user.location
-    )
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
