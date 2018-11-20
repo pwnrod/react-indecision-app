fa8ab0979a232c651f96e0238af5c4dcc45d838b
Caylin James <caylinjames.dev@gmail.com>
2018-11-20 07:52:12 -0500

    more working with JSX expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,68 @@
 
 console.log('App.js is running!');
 
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer'
+};
+
 // JSX - JavaScript XML
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
 );
+
+var user = {
+    name: 'Caylin',
+    age: 28,
+    location: 'Louisville, KY'
+};
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
