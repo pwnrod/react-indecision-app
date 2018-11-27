5127485ae6eb5f00eed56e3c3876faf161610007
Caylin James <caylinjames.dev@gmail.com>
2018-11-27 07:38:40 -0500

    es6 classes - part 1

---
+++ src/playground/es6-classes-1.js
@@ -0,0 +1,21 @@
+
+class Person {
+    constructor(name = 'Anonymous', age = 0) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getGreeting() {
+        return `Hi. I am ${this.name}!`;
+    }
+
+    getDescription() {
+        return `${this.name} is ${this.age} years old.`;
+    }
+}
+
+const me = new Person('Caylin James', 28);
+console.log(me.getDescription());
+
+const other = new Person();
+console.log(other.getDescription());
