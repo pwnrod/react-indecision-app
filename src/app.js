6be0e29d841f0ccb78e866fb8408147b868423c5
Caylin James <caylinjames.dev@gmail.com>
2018-11-28 07:25:35 -0500

    nesting components

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 
+class IndecisionApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Action />
+                <Options />
+                <AddOption />
+            </div>
+        );
+    }
+}
+
 class Header extends React.Component {
     render() {
         return (
@@ -25,6 +38,17 @@ class Options extends React.Component {
         return (
             <div>
                 <p>Options component here</p>
+                <Option />
+            </div>
+        );
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Option component here</p>
             </div>
         );
     }
@@ -40,13 +64,4 @@ class AddOption extends React.Component {
     }
 }
 
-const jsx = (
-    <div>
-        <Header/>
-        <Action/>
-        <Options/>
-        <AddOption/>
-    </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
