6f293d809d51defe69c4bf0d7d0753ffd7878869
Caylin James <caylinjames.dev@gmail.com>
2019-01-03 13:18:01 -0500

    es6 import/export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('app.js is running!!!');
+import {isAdult, canDrink} from './person.js';
+
+console.log(isAdult(17));
+console.log(isAdult(20));
+console.log(canDrink(20));
+console.log(canDrink(22));
---
+++ src/utils.js
@@ -0,0 +1,4 @@
+console.log('utils.js is running too');
+
+export const square = (x) => x * x;
+export const add = (a, b) => a + b;
