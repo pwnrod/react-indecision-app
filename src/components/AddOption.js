a9b03e13164885315657b4433ab468ef19e999d5
Caylin James <caylin@lougeek.com>
2019-01-04 15:16:19 -0500

    add webpack dev-server

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -11,7 +11,6 @@ export default class AddOption extends React.Component {
 
     handleAddOption(e) {
         e.preventDefault();
-        console.log(testingg);
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-console.log(__dirname);
-
 module.exports = {
     entry: './src/app.js',
     output: {
@@ -15,5 +13,8 @@ module.exports = {
             exclude: /node_modules/
         }]
     },
-    devtool: "cheap-module-eval-source-map"
+    devtool: "cheap-module-eval-source-map",
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
 };
