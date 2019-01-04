62314df76b2bb571c742a89784d3c46c8639d66c
Caylin James <caylinjames.dev@gmail.com>
2019-01-04 07:01:51 -0500

    default exports

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
-import {isAdult, canDrink} from './person.js';
+// import subtract, {square, add} from './utils.js';
 
-console.log(isAdult(17));
-console.log(isAdult(20));
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(100, 23));
+// console.log(subtract(100, 81));
+
+
+import isSenior, {isAdult, canDrink} from "./person";
+console.log(isAdult(18));
 console.log(canDrink(20));
-console.log(canDrink(22));
+console.log(isSenior(65));
