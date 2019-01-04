d838d0e616b4e5f6f831d82233a5a5cb623e6110
Caylin James <caylinjames.dev@gmail.com>
2019-01-04 07:23:18 -0500

    setting up babel with webpack

--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
-// import subtract, {square, add} from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 81));
+const template = <p>THIS IS JSX FROM WEBPACK</p>;
 
-
-import isSenior, {isAdult, canDrink} from "./person";
-console.log(isAdult(18));
-console.log(canDrink(20));
-console.log(isSenior(65));
+ReactDOM.render(template, document.getElementById('app'));
